3c9cdd5abe77de25b71c580b4217c564ab890fe4
David John Gagne <djgagne@ou.edu>
2017-04-12 15:05:58 -0600

    Updated setup file

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
           author_email="dgagne@ucar.edu",
           license="MIT",
           url="https://github.com/djgagne/deepsky",
-          packages=["deepsky"],
-          install_requires=["matplotlib",
-                            "numpy",
-                            "scipy",
-                            "pandas",
-                            "xarray",
-                            "keras"])
+          packages=["deepsky"])
+#          install_requires=["matplotlib",
+#                            "numpy",
+#                            "scipy",
+#                            "pandas",
+#                            "xarray",
+#                            "keras"])
